23420c0a090690a46bf21a9223d1507ca50e91b0
saloniJat1707 <salonij.bca2025@ssism.org>
2026-03-20 21:37:45 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,22 @@
+function App() {
+  return (
+    <div>
+
+      <h1>Hello Saloni 👋</h1>
+      <p>Welcome to my Portfolio 🚀</p>
+
+      <h2>About</h2>
+      <p>I am learning currently React and building websites.</p>
+
+      <h2>Projects</h2>
+      <p>1. Portfolio Website</p>
+      <p>2. Nursery Website</p>
+
+      <h2>Contact</h2>
+    <p>Email: Salonijat1707@gmail.com</p>
+
+    </div>
+  );
+}
+
+export default App;
